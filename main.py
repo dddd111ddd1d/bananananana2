a1d86a9afaf0fef01e20bc3c5bfb231b2cf5b09a
dddd111ddd1d <125788795+dddd111ddd1d@users.noreply.github.com>
2023-03-26 06:51:05 +0000

    added information about product

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://rozetka.com.ua/ua/outward_hound_pt67835/p195264728/"
+URL = "https://www.olx.ua/d/uk/obyavlenie/prodam-banan-kievskiy-super-karlik-IDC7KUN.html"
 data = requests.get(URL).text
 
 soup = BeautifulSoup()
+
+product_name = soup.find_all("h1", {"class":"css-1soizd2 er34gjf0"})
+product_price = soup.find_all("h3", {"class":"css-ddweki er34gjf0"})
+product_description = soup.find_all("div", {"class":"css-bgzo2k er34gjf0"})
+Similar_ads = soup.find_all("div", {"class":"css-1wbpwse"})
+print("Назва продукта -", product_name)
+print("Ціна продукта -", product_price)
+print("Опис продукта -", product_description)
+print("Схожі товари -", Similar_adds)
+                  
